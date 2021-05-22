12d8628354b5d5144d5bf780de315ada1f2b1128
brucenelson6655 <bruce.rugger@gmail.com>
2021-05-22 15:02:02 -0700

    initial commit

---
+++ HiveClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Odbc;
+using System.Data.Common;
+
+namespace HiveClient
+{
+    class Program
+  {
+    static void Main(string[] args)
+    {
+      GetData();
+      Console.WriteLine("----------------------------------------------");
+      Console.WriteLine("Press a key to end");
+      Console.Read();
+    }
+
+    static async void GetData()
+    {
+      using (OdbcConnection conn = 
+             new OdbcConnection("DSN=sqla;UID=token;PWD=<YOUR PAT TOKEN>"))
+      {
+        conn.OpenAsync().Wait();
+        OdbcCommand cmd = conn.CreateCommand();
+        cmd.CommandText =
+            "select * from customers limit 10;";
+        DbDataReader dr = await cmd.ExecuteReaderAsync();
+        while (dr.Read())
+        {
+          Console.WriteLine(dr.GetString(1)); 
+        }
+      }
+    }
+  }
+}
